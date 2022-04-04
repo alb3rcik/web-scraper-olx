706c0ddb98811f721e7599baeef23b43ecee952c
alb3rcik <apieniadz@student.agh.edu.pl>
2022-04-04 14:20:41 +0200

    commit

--- projekt.py
+++ projekt.py
@@ -1,18 +1,47 @@
 from bs4 import BeautifulSoup
 from requests import get
 
-URL="https://www.olx.pl/oferty/q-drążek-do-podciągania/"
-page=get(URL)
-bs=BeautifulSoup(page.content, 'html.parser')
-for offer in bs.find_all('div', class_='offer-wrapper'):
-    footer=offer.find('td', class_='bottom-cell')
+page = 1
+liczba_ofert = 1
+while page != 25:
+    URL = f"https://www.olx.pl/oferty/q-drążek-do-podciągania/?page={page}"
+    #print(URL)
+    #page=get(URL)
+    #bs=BeautifulSoup(strona.content, 'html.parser')
+    page = page + 1
+    strona=get(URL)
+    bs=BeautifulSoup(strona.content, 'html.parser')
+    #liczba_ofert = 0
+    for offer in bs.find_all('div', class_='offer-wrapper'):
+        footer=offer.find('td', class_='bottom-cell')
+        #print(footer)
+        title=offer.find('strong').get_text()
+        location=footer.find('small', class_="breadcrumb x-normal").get_text().strip().split(",")[0]
+        price=offer.find("p", class_="price").get_text().strip()
+        #print(location)
+        if location =="Kraków":
+            print(location,title,price)
+            liczba_ofert +=1
+print(liczba_ofert)
+    
+
+    #sprawdzam czy działa git na surface
+    #print(title)
+    #print(price)
+
+#URL="https://www.olx.pl/oferty/q-drążek-do-podciągania/"
+#page=get(URL)
+#bs=BeautifulSoup(page.content, 'html.parser')
+
+#for offer in bs.find_all('div', class_='offer-wrapper'):
+ #   footer=offer.find('td', class_='bottom-cell')
     #print(footer)
-    title=offer.find('strong').get_text()
-    location=footer.find('small', class_="breadcrumb x-normal").get_text().strip().split(",")[0]
-    price=offer.find("p", class_="price").get_text().strip()
+  #  title=offer.find('strong').get_text()
+   # location=footer.find('small', class_="breadcrumb x-normal").get_text().strip().split(",")[0]
+    #price=offer.find("p", class_="price").get_text().strip()
     #print(location)
-    if location =="Kraków":
-        print(location,title,price)
+    #if location =="Kraków":
+     #   print(location,title,price)
 
     #sprawdzam czy działa git na surface
     #print(title)
