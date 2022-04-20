b507d9f44748ac6ec41a9f7bd4c2a0904751c31c
alb3rcik <apieniadz@student.agh.edu.pl>
2022-04-21 00:05:37 +0200

    commit

--- projekt.py
+++ projekt.py
@@ -1,4 +1,3 @@
-from tokenize import Number
 from bs4 import BeautifulSoup
 from requests import get
 import sqlite3
@@ -22,18 +21,18 @@ if len(argv)>1 and argv[1]=="setup":
     cursor.execute("CREATE TABLE offers(name TEXT, price REAL, city TEXT)")
     quit()
 
-page = 1
-liczba_ofert = 0
+#page = 1
 
-#while page != 25:
+#liczba_ofert = 0
+
 def parse_page(page):
-    #print(f"Pracuje nad strona numer {page}")
+    global liczba_ofert
+    print(f"Pracuje nad strona numer {page}")
     URL = f"https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/malopolskie//?page={page}" 
     #URL = f"https://www.olx.pl/oferty/q-drążek-do-podciągania//?page={page}" 
     page = page + 1
     strona=get(URL)
     bs=BeautifulSoup(strona.content, 'html.parser')
-    liczba_ofert = 0
     for offer in bs.find_all('div', class_='offer-wrapper'):
         footer=offer.find('td', class_='bottom-cell')
     
@@ -44,19 +43,22 @@ def parse_page(page):
             price=parse_price(offer.find("p", class_="price").get_text().strip())
         except:
             print("brak ceny")
-        #if location=="Kraków" and price<250000.0:
+        
         cursor.execute("INSERT INTO offers VALUES (?,?,?)", (title,price,location))
         db.commit()
             #print(location,title,price)
-        liczba_ofert+=1
+        #liczba_ofert+=1
         if location=="Kraków" and price<150000.0:
-           print(location,title,price)
-            #liczba_ofert +=1
+            #liczba_ofert+=1
+            print(location,title,price)
+       
+#print(liczba_ofert)
+                
 db=sqlite3.connect("dane.db")
 cursor=db.cursor() 
 
-#python projekt.py setup
 
+#print(liczba_ofert)
 
 if len(argv)>1 and argv[1]=="setup":
     cursor.execute("CREATE TABLE offers(name TEXT, price REAL, city TEXT)")
@@ -68,29 +70,3 @@ for page in range(1,26):
 
 
 db.close()
-#print(liczba_ofert)
-#print(page)
-
-#print(liczba_ofert)
-
-    #sprawdzam czy działa git na surface
-    #print(title)
-    #print(price)
-
-#URL="https://www.olx.pl/oferty/q-drążek-do-podciągania/"
-#page=get(URL)
-#bs=BeautifulSoup(page.content, 'html.parser')
-
-#for offer in bs.find_all('div', class_='offer-wrapper'):
- #   footer=offer.find('td', class_='bottom-cell')
-    #print(footer)
-  #  title=offer.find('strong').get_text()
-   # location=footer.find('small', class_="breadcrumb x-normal").get_text().strip().split(",")[0]
-    #price=offer.find("p", class_="price").get_text().strip()
-    #print(location)
-    #if location =="Kraków":
-     #   print(location,title,price)
-
-    #sprawdzam czy działa git na surface
-    #print(title)
-    #print(price)
