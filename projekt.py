c3a477cb7720479dd3b17f3d34a29d40261a7429
alb3rcik <apieniadz@student.agh.edu.pl>
2022-04-21 14:53:51 +0200

    commit

--- projekt.py
+++ projekt.py
@@ -1,4 +1,3 @@
-from tokenize import Number
 from bs4 import BeautifulSoup
 from requests import get
 import sqlite3
@@ -66,31 +65,4 @@ if len(argv)>1 and argv[1]=="setup":
 for page in range(1,26):
     parse_page(page)
 
-
 db.close()
-#print(liczba_ofert)
-#print(page)
-
-#print(liczba_ofert)
-
-    #sprawdzam czy działa git na surface
-    #print(title)
-    #print(price)
-
-#URL="https://www.olx.pl/oferty/q-drążek-do-podciągania/"
-#page=get(URL)
-#bs=BeautifulSoup(page.content, 'html.parser')
-
-#for offer in bs.find_all('div', class_='offer-wrapper'):
- #   footer=offer.find('td', class_='bottom-cell')
-    #print(footer)
-  #  title=offer.find('strong').get_text()
-   # location=footer.find('small', class_="breadcrumb x-normal").get_text().strip().split(",")[0]
-    #price=offer.find("p", class_="price").get_text().strip()
-    #print(location)
-    #if location =="Kraków":
-     #   print(location,title,price)
-
-    #sprawdzam czy działa git na surface
-    #print(title)
-    #print(price)
