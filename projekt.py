21fb674af06737fe19274afcca5cba5b298d331a
alb3rcik <apieniadz@student.agh.edu.pl>
2022-04-21 15:56:02 +0200

    commit

--- projekt.py
+++ projekt.py
@@ -21,16 +21,12 @@ if len(argv)>1 and argv[1]=="setup":  #jeśli w terminalu wpiszemy setup (drugi 
     cursor.execute("CREATE TABLE offers(name TEXT, price REAL, city TEXT)") #tworzymy baze danych "offers", o kolumnach podanych
     quit()
 
-#page = 1
-
-#liczba_ofert = 0
-
 def parse_page(page):                                            #definiujemy sobie funkcje która parsuje nam stronę
     global liczba_ofert
     print(f"Pracuje nad strona numer {page}")                    #po każdym parsowaniu jednej strony wypisujemy ten komunikat
     URL = f"https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/malopolskie//?page={page}" 
     #URL = f"https://www.olx.pl/oferty/q-drążek-do-podciągania//?page={page}" 
-    #page = page + 1
+    
     strona=get(URL)     #definiujemy zmienną strona która pobiera zawartość strony przy pomocy metody get
     bs=BeautifulSoup(strona.content, 'html.parser') #definiujemy zmienną której zawartością jest nasza strona robimy to za pomocą metody .content
     
@@ -39,35 +35,23 @@ def parse_page(page):                                            #definiujemy so
         title=offer.find('strong').get_text() #tytuł pobieramy ze znacznika "strong" i pobieramy sam tekst
         location=footer.find('small', class_="breadcrumb x-normal").get_text().strip().split(",")[0] #definiujemy zmienną location, która zawierać będzie lokalizację pobieramy z niej sam tekst i następnie za pomocą .strip usuwamy znaki nowej lini, a następnie rozdzielamy Miasto od dzielnicy (np. Kraków, Podgórze) i zostawiam samo miasto
         try: #używamy instrukcji try except 
-            price=parse_price(offer.find("p", class_="price").get_text().strip())
+            price=parse_price(offer.find("p", class_="price").get_text().strip()) 
         except:
             print("brak ceny")
         
-        cursor.execute("INSERT INTO offers VALUES (?,?,?)", (title,price,location))
-        db.commit()
+        cursor.execute("INSERT INTO offers VALUES (?,?,?)", (title,price,location)) #do naszej kolumn wrzucamy dane title price i location
+        db.commit()    #zapisujemy zmiany
             #print(location,title,price)
-        #liczba_ofert+=1
-        if location=="Kraków" and price<150000.0:
-            #liczba_ofert+=1
+       
+        if location=="Kraków" and price<150000.0: #dodatkowo możemy sobie wyszukiwać konkretne miejscowości wraz z ceną i zostaną wyprintowane w terminalu
             print(location,title,price)
        
-#print(liczba_ofert)
-                
-#db=sqlite3.connect("dane.db")
-#cursor=db.cursor() 
 
-
-#print(liczba_ofert)
-
-if len(argv)>1 and argv[1]=="setup":
-    cursor.execute("CREATE TABLE offers(name TEXT, price REAL, city TEXT)")
-    quit()
-
-
-for page in range(1,26):
+for page in range(1,26): #parsujemy strona po stronie
     parse_page(page)
 
 
-db.close()
+db.close() #zamykamy połączenie z bazą danych musimy pamiętać o zcomittowaniu bo inaczej dane zostaną usunięte
+
 
 
